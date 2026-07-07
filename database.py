cca7998fcbd459149da87b755731044aab46cecb
lcarlo23 <160224871+lcarlo23@users.noreply.github.com>
2026-07-07 10:29:12 +0200

    Add db functions to add and retrieve employees

--- database.py
+++ database.py
@@ -1,19 +1,75 @@
 import sqlite3
 
 
-def initialize_db():
-    # Connect to the database
+def create_connection():
+    # Connect to the db
     connection = sqlite3.connect("directory.db")
     cursor = connection.cursor()
 
-    # Create table if not already existing
+    return connection, cursor
+
+
+def initialize_db():
+    # Connect db
+    connection, cursor = create_connection()
+
+    # Create table if not already exists
     cursor.execute("""CREATE TABLE IF NOT EXISTS employees(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     role TEXT,
     salary REAL
     )""")
-    connection.commit()
 
-    # Close connection to db
+    # Commit and close db
+    connection.commit()
     connection.close()
+
+
+def add_employee(name, role, salary):
+    # Connect db
+    connection, cursor = create_connection()
+
+    # Add employee to db
+    cursor.execute(
+        """INSERT INTO employees (
+                   name,
+                   role,
+                   salary
+                   ) VALUES (
+                   ?,
+                   ?,
+                   ?
+                   )""",
+        (name, role, salary),
+    )
+
+    # Commit and close db
+    connection.commit()
+    connection.close()
+
+
+def get_all_employees():
+    # Connect db
+    connection, cursor = create_connection()
+
+    # Retrieve employees
+    cursor.execute("""SELECT * FROM employees""")
+    employees = cursor.fetchall()
+
+    # Close db and return employees
+    connection.close()
+    return employees
+
+
+def get_employee(employee_id):
+    # Connect db
+    connection, cursor = create_connection()
+
+    # Retrieve employee
+    cursor.execute("""SELECT * FROM employees WHERE id = ? """, (employee_id,))
+    employee = cursor.fetchall()
+
+    # Close db and return employees
+    connection.close()
+    return employee
