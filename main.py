9aaf99bae8b7c0051ac2242c94900c6fdd684c75
lcarlo23 <160224871+lcarlo23@users.noreply.github.com>
2026-07-07 11:12:09 +0200

    Add basic menu system

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-from database import initialize_db
+import database as db
 
-initialize_db()
+# Check if database exists or create a new one
+db.initialize_db()
+
+
+# CLI menu
+
+while True:
+    print("### Welcome to Staff Tracker ###")
+    print()
+    print("1. Add new employee")
+    print("2. Edit employee")
+    print("3. Delete employee")
+    print("4. Get all employees")
+    print("5. Get an employee")
+    print("6. Exit")
+    print()
+
+    selection = int(input("Select an option (1-6): "))
+
+    if selection == 6:
+        break
